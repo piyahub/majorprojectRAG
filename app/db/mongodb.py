f8658385e74f7c450637b36debdca9600220d285
Priya Saxena <76806897+piyahub@users.noreply.github.com>
2026-01-10 17:44:44 +0530

    Refactor MongoDB client connection settings
    Updated MongoDB client connection with timeout settings and changed database name to use settings.

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -1,7 +1,21 @@
+# from pymongo import MongoClient
+# from app.core.config import settings
+
+# client = MongoClient(settings.MONGO_URI)
+# db = client["simple_rag"]
+
+# documents_collection = db["documents"]
+
+
+
 from pymongo import MongoClient
 from app.core.config import settings
 
-client = MongoClient(settings.MONGO_URI)
-db = client["simple_rag"]
+client = MongoClient(
+    settings.MONGO_URI,
+    connectTimeoutMS=60000,   # 60s
+    socketTimeoutMS=60000,    # 60s
+    serverSelectionTimeoutMS=60000
+)
 
-documents_collection = db["documents"]
+db = client[settings.MONGO_DB_NAME]
