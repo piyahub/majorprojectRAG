dc69c3cedfc9f0478251e7b286b2098b9a09672f
Priya Saxena <76806897+piyahub@users.noreply.github.com>
2026-01-10 17:38:35 +0530

    Comment out startup event for document ingestion
    Comment out the startup event to prevent document ingestion.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+
 from fastapi import FastAPI
 from app.api.v1.routes.chat import router as chat_router
-from app.services.chroma_service import ingest_documents_to_chroma
+
 from app.core.config import settings
 from dotenv import load_dotenv
 
@@ -8,14 +9,17 @@ load_dotenv()
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-@app.on_event("startup")
-def startup_event():
-    ingest_documents_to_chroma()
+# @app.on_event("startup")
+# def startup_event():
+#     ingest_documents_to_chroma()
 
 app.include_router(chat_router)
 
 @app.get("/")
 def root():
     return {"message": "Vector RAG Chatbot Running"}
+
+
+
 
 
