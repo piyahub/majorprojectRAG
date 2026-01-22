1c37984999c9759f6366173012a03155d11c487d
Priya Saxena <76806897+piyahub@users.noreply.github.com>
2026-01-22 22:16:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from fastapi import FastAPI
 from app.api.v1.routes.chat import router as chat_router
-
+from app.api.v1.routes.admin import router as admin_router
 from app.core.config import settings
 from dotenv import load_dotenv
 
@@ -14,7 +14,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 #     ingest_documents_to_chroma()
 
 app.include_router(chat_router)
-
+app.include_router(admin_router)
 @app.get("/")
 def root():
     return {"message": "Vector RAG Chatbot Running"}
